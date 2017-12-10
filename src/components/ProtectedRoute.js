71961461230a04d2fdf1d373ad53c6377578103a
zhufengnodejs <zhufengnodejs@126.com>
2017-12-10 16:18:05 +0800

    受保护的页面

--- src/components/ProtectedRoute.js
+++ src/components/ProtectedRoute.js
@@ -9,11 +9,15 @@ import {Route,Redirect} from 'react-router-dom';
  * component = 组件
  * render 是一个函数，返回值是一个组件
  * 如果当前用户已登录，则直接渲染受保护的页面，如果未登录，则重定向到登录页，并传递过去当前路径
+ * 路由渲染出来的组件三个属性
+ * history Router给他的 context
+ * location
+ * match
  */
 export default function({component:Component,...rest}){
   return (
-    <Route {...rest} render={({location})=>(
-      localStorage.getItem('login')?<Component/>:<Redirect to={{pathname:'/login',state:{from:location.pathname}}}/>
+    <Route {...rest} render={({history,location,match})=>(
+      localStorage.getItem('login')?<Component />:<Redirect to={{pathname:'/login',state:{from:location.pathname}}}/>
     )} />
   )
 }
