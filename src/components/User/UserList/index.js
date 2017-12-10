1810f3fadd59b305c9b44a59b91101a0ec230b03
zhufengnodejs <zhufengnodejs@126.com>
2017-12-10 12:25:17 +0800

    构建空的完整开发环境

--- src/components/User/UserList/index.js
+++ src/components/User/UserList/index.js
@@ -1,11 +1,25 @@
 import React, {Component} from 'react';
 import {Link,Route} from 'react-router-dom';
 export default class UserList extends Component {
+  constructor(){
+    super();
+    this.state = {users:[]};
+  }
+  componentDidMount(){
+    let users = localStorage.getItem('users')?JSON.parse(localStorage.getItem('users')):[];
+    this.setState({users});
+  }
   render() {
     return (
-      <div>
-        UserList
-      </div>
+      <ul className="list-group">
+        {
+          this.state.users.map(item=>(
+            <li key={item.id} className="list-group-item">
+              {item.username}
+            </li>
+          ))
+        }
+      </ul>
     )
   }
 }
